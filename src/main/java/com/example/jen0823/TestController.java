2bcdabe3246dfe58cd17f562cf265095d1cd3c03
eunjooNine <eunjoo.nine@gmail.com>
2024-08-23 16:56:56 +0900

    jenkins test5

--- src/main/java/com/example/jen0823/TestController.java
+++ src/main/java/com/example/jen0823/TestController.java
@@ -10,7 +10,7 @@ public class TestController {
 
     @RequestMapping(method = RequestMethod.GET, value = "/test")
     public String test() {
-        return "v4";
+        return "v5";
     }
 
 }
